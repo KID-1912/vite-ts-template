64e0e62bd3ad20fa938a4f71609a2291a329c225
heyutongxue <heyutongxue@gmail.com>
2024-09-27 19:39:49 +0800

    feat:tsconfig.json

--- src/components/base/SvgIcon/SvgIcon.ts
+++ src/components/base/SvgIcon/SvgIcon.ts
@@ -1,19 +1,19 @@
 import { defineComponent, defineAsyncComponent, h } from "vue";
 import type { AsyncComponentLoader } from "vue";
 
-const svgModules = import.meta.glob("./svg/**/*.svg", {
-  query: "?component",
-});
+interface SvgModules {
+  [key: string]: AsyncComponentLoader;
+}
+
+const svgModules: SvgModules = import.meta.glob("./svg/**/*.svg", { query: "?component" });
 
 export default defineComponent({
   name: "SvgIcon",
   props: {
-    name: { type: String, default: "" },
+    name: { type: String, required: true },
   },
   setup(props) {
-    const SvgComponent = defineAsyncComponent(
-      svgModules[`./svg/${props.name}.svg`] as AsyncComponentLoader,
-    );
+    const SvgComponent = defineAsyncComponent(svgModules[`./svg/${props.name}.svg`]);
     return () => h(SvgComponent);
   },
 });
--- src/components/base/SvgIcon/SvgIcon.vue
+++ src/components/base/SvgIcon/SvgIcon.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script lang="ts" setup>
 import { defineAsyncComponent } from "vue";
 const props = defineProps({
   name: { type: String, default: "" },
